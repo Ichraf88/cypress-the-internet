03352312b1d2a1fa5189836ff7b331b9d3f1cb47
Ichraf88 <ichrafbenmohamed@gmail.com>
2025-06-02 19:20:47 -0400

    Ajout dun test Cypress de connexion

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,5 +2,6 @@ describe('Test de base', () => {
   it('affiche le site', () => {
     cy.visit('https://the-internet.herokuapp.com/login')
     cy.contains('Login Page').should('be.visible')
+    cy.contains('Login Page').should('be.enable')
   })
 })
